9473051c7c4dd67ed67cafdc5929ef8ccb8a4ae9
Jesus Castañeda Espinosa <99563848+Akir91@users.noreply.github.com>
2023-04-12 09:00:23 -0600

    Condicionales parte 2

---
+++ CondicionalMultiple.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class CondicionalMultiple
+{
+    public static void main(String [] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Introduce tu edad: ");
+        byte edad = sc.nextByte();
+        
+        if(edad >= 21)
+        {
+            System.out.println("Eres mayor de edad en MEX y USA");
+        }
+        
+        else if(edad >= 18)
+        {
+            System.out.println("Eres mayor de edad en MEX pero no en USA");
+        }
+        
+        else
+        {
+            System.out.println("Eres menor de edad en MEX y USA");
+        }            
+        
+    }
+    
+}
