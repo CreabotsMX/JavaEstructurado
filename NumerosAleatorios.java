ab665a204fe36ca8930eb76a2039cfcd6fc81a1d
Jesus Castañeda Espinosa <99563848+Akir91@users.noreply.github.com>
2024-02-26 00:06:32 -0600

    Update NumerosAleatorios.java
    Add validation non-repeating numbers

--- NumerosAleatorios.java
+++ NumerosAleatorios.java
@@ -1,6 +1,8 @@
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 
-public class NumerosAleatorios 
+public class Main 
 {
     public static void main(String[] args) 
     {
@@ -12,13 +14,30 @@ public class NumerosAleatorios
         System.out.print("Ingresa la cantidad de preguntas: ");
         byte preguntas = sc.nextByte();
         
+        System.out.print("Ingresa el limite inferior para generar numeros aleatorios: ");
+        int limiteInferior = sc.nextInt();
+        
+        System.out.print("Ingresa el limite superior para generar numeros aleatorios: ");
+        int limiteSuperior = sc.nextInt();
+        
+        int diferencia = limiteSuperior - limiteInferior + 1;
+        
         for(byte alumno = 1; alumno < alumnos + 1; alumno++)
         {
             System.out.print(alumno + ".\t");
             
+            Set<Integer> preguntasGeneradas = new HashSet<>();
+            
             for(byte pregunta = 1; pregunta < preguntas+1; pregunta++)
             {
-                System.out.print((int)(Math.random()*45+19) + "\t");
+                int preguntaAleatoria;
+                do {
+                    preguntaAleatoria = (int)(Math.random() * diferencia + limiteInferior);
+                } while (preguntasGeneradas.contains(preguntaAleatoria));
+                
+                preguntasGeneradas.add(preguntaAleatoria);
+                
+                System.out.print(preguntaAleatoria + "\t");
             }
             System.out.println("");
         }
