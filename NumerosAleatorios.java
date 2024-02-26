8e2bee768674e9ef1df6be3d916dda15e104a963
Jesus Castañeda Espinosa <99563848+Akir91@users.noreply.github.com>
2024-02-25 23:46:42 -0600

    Update NumerosAleatorios.java
    Add function to request input data

--- NumerosAleatorios.java
+++ NumerosAleatorios.java
@@ -1,20 +1,26 @@
-package ejerciciose2;
+import java.util.Scanner;
 
-public class EjerciciosE2 
+public class NumerosAleatorios 
 {
-
     public static void main(String[] args) 
     {
-        for(byte alumno = 1; alumno <31; alumno++)
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Ingresa la cantidad de alumnos: ");
+        byte alumnos = sc.nextByte();
+
+        System.out.print("Ingresa la cantidad de preguntas: ");
+        byte preguntas = sc.nextByte();
+        
+        for(byte alumno = 1; alumno < alumnos + 1; alumno++)
         {
             System.out.print(alumno + ".\t");
             
-            for(byte preguntas = 1; preguntas <6; preguntas++)
+            for(byte pregunta = 1; pregunta < preguntas+1; pregunta++)
             {
                 System.out.print((int)(Math.random()*45+19) + "\t");
             }
             System.out.println("");
         }
     }
-    
 }
